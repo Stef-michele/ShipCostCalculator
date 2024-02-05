bc3a623c32a0c80fc7fb70e7bdd088b00ffc125d
Stefanie Gay <stefanie.gay@gmail.com>
2024-02-04 19:55:39 -0500

    First Draft

---
+++ src/ShipCostCalculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ShipCostCalculator
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in); //calling scanner to program
+        double itemPrice = 0;
+        double shippingCost = 0;
+        double totalCost = 0;
+        //Prompt user to assign itemPrice
+        System.out.print("Enter the item price: $ ");
+        itemPrice = in.nextDouble();
+        //calculate shipping and give appropriate output
+        if (itemPrice >= 100) {
+            shippingCost = 0;
+            System.out.println(" Your shipping is: $" + shippingCost + "Your total cost is: $" + itemPrice);
+        } else {
+            shippingCost = 0.02 * itemPrice;
+            totalCost = itemPrice + shippingCost;
+            // output results for charged shipping
+            System.out.println(" Your shipping is: " + shippingCost + "Your total cost is: $" + totalCost);
+        }
+    }
+}
