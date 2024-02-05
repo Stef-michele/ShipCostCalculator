ae441dfa1e28fd6b7778812a71ac56f6016e2f53
Stefanie Gay <stefanie.gay@gmail.com>
2024-02-05 13:22:00 -0500

    Finished revised no scanner

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -1,27 +1,29 @@
-import java.util.Scanner;
+
 public class ShipCostCalculator
 {
     public static void main(String[] args)
-    {
-        Scanner in = new Scanner(System.in); //calling scanner to program
-        double itemPrice = 0;
+    {   double itemPrice = 0;
         double shippingCost = 0;
         double totalCost = 0;
+        String trash = "";
         //Prompt user to assign itemPrice
-        System.out.print("Enter the item price: $ ");
-        itemPrice = in.nextDouble();
-        //calculate shipping and give appropriate output
-        if (itemPrice >= 100) {
-            shippingCost = 0;
-            System.out.println(" Your shipping is: $ " + shippingCost);
-            totalCost = itemPrice + shippingCost;
-            System.out.print("Your total cost is: $" + totalCost);
-        } else {
-            shippingCost = 0.02 * itemPrice;
-            totalCost = itemPrice + shippingCost;
-            // output results for charged shipping
-            System.out.println(" Your shipping is: $" + shippingCost);
-            System.out.print("Your total cost is: $" + totalCost);
+        System.out.println("Enter the item price: ");
+        // fake scanner input
+        itemPrice = 100.25;
+        //complete program
+
+        {
+            if (itemPrice >= 100) {
+                shippingCost = 0;
+                totalCost = itemPrice;
+            } else {
+                shippingCost = 0.02 * itemPrice;
+                totalCost = itemPrice + shippingCost;
+            }
+                // output results for charged shipping
+                System.out.println("Your shipping is: $" + shippingCost);
+                System.out.print("Your total cost is: $" + totalCost);
+
         }
     }
 }
